2d0d68697c14d15dbebaab02760b4aadd17bb469
RedCat <redcat@nxt.ru>
2019-09-13 03:04:04 +0500

    Change license type

--- evo/plugins/MaxMindIpInfo/assets/plugins/MaxMindIpInfo/MaxMindIpInfo.php
+++ evo/plugins/MaxMindIpInfo/assets/plugins/MaxMindIpInfo/MaxMindIpInfo.php
@@ -6,15 +6,15 @@
  *
  * @category    plugin
  * @version     1.0.1
- * @license     http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL) for pligin
+ * @license     https://creativecommons.org/licenses/by-sa/4.0/ Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) for MaxMind data
  * @internal    @properties &display=Collect IP Info;list;All,City,Country,ASN;All
  * @internal    @events OnBeforeLoadDocumentObject
  * @internal    @modx_category Manager and Admin
  * @internal    @legacy_names MaxMindIpInfo
  * @internal    @installset base
  *
- * @reportissues https://github.com/extras-evolution/tinymce4-for-modx-evo
- * @documentation Plugin docs https://github.com/extras-evolution/tinymce4-for-modx-evo
+ * @documentation https://github.com/oldredcat/ModX/tree/master/evo/plugins/MaxMindIpInfo
  * @author      RedCat
  * @lastupdate  2019-09-13
  */
